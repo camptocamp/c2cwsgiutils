91f74885f3535ea70ca5027ce592a75414016ee6
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2017-03-27 09:03:22 +0200

    Can now stop the WSGI app without kill -9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = '0.5.0'
+VERSION = '0.5.1'
 HERE = os.path.abspath(os.path.dirname(__file__))
 INSTALL_REQUIRES = open(os.path.join(HERE, 'rel_requirements.txt')).read().splitlines()
 

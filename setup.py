6cb6433259213389d7f05ca59e520bcb03761b8d
Patrick Valsecchi <patrick@thus.ch>
2017-04-25 15:13:21 +0200

    Add license

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = '0.9.1'
+VERSION = '0.9.2'
 HERE = os.path.abspath(os.path.dirname(__file__))
 INSTALL_REQUIRES = open(os.path.join(HERE, 'rel_requirements.txt')).read().splitlines()
 
@@ -25,7 +25,7 @@ setup(
     author='Camptocamp',
     author_email='info@camptocamp.com',
     url='https://github.com/camptocamp/c2cwsgiutils',
-    license='MIT',
+    license='FreeBSD',
     packages=find_packages(exclude=['ez_setup', 'acceptance_tests', 'tests', 'doc']),
     include_package_data=True,
     zip_safe=False,

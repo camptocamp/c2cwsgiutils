b90b0f268099e212eac664200d8e29e3b305e4d6
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2017-05-29 09:04:30 +0200

    Add timeout to Composition.run and exec

--- c2cwsgiutils/acceptance/composition.py
+++ c2cwsgiutils/acceptance/composition.py
@@ -83,15 +83,15 @@ class Composition(object):
                                    'restart', '%s_%s_1' % (self.project_name, container)],
                                    stderr=subprocess.STDOUT))
 
-    def run(self, container, *command):
+    def run(self, container, timeout=None, *command):
         subprocess.check_call(['docker-compose', '--file', self.composition,
                                '--project-name', self.project_name, 'run', '--rm', container] + list(command),
-                              env=Composition._get_env(), stderr=subprocess.STDOUT)
+                              env=Composition._get_env(), stderr=subprocess.STDOUT, timeout=timeout)
 
-    def exec(self, container, *command):
+    def exec(self, container, timeout=None, *command):
         subprocess.check_call(['docker-compose', '--file', self.composition,
                                '--project-name', self.project_name, 'exec', '-T', container] + list(command),
-                              env=Composition._get_env(), stderr=subprocess.STDOUT)
+                              env=Composition._get_env(), stderr=subprocess.STDOUT, timeout=timeout)
 
     @staticmethod
     def _get_env():

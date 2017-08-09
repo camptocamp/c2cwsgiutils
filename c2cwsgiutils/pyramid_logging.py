9342b02afe5fa113ec0d18fdb1710bfeb2468c98
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2017-08-09 10:18:41 +0200

    Add referer info to the JSON logs

--- c2cwsgiutils/pyramid_logging.py
+++ c2cwsgiutils/pyramid_logging.py
@@ -35,6 +35,8 @@ class _PyramidFilter(logging.Filter):
                 record.matched_route = request.matched_route.name
             record.path = request.path
             record.request_id = request.c2c_request_id
+            if request.referer is not None:
+                record.referer = request.referer
         record.level_name = record.levelname
         return True
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = '0.19.0'
+VERSION = '0.19.1'
 HERE = os.path.abspath(os.path.dirname(__file__))
 INSTALL_REQUIRES = open(os.path.join(HERE, 'requirements.txt')).read().splitlines()
 

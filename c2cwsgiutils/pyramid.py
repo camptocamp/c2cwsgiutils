d5ee931e04b74b3a3744c6386a16df33a84e306e
Patrick Valsecchi <patrick@thus.ch>
2017-09-28 16:37:42 +0200

    Change the JSON renderer to indent conditionally
    Closes #14

--- c2cwsgiutils/pyramid.py
+++ c2cwsgiutils/pyramid.py
@@ -2,7 +2,7 @@ import cornice
 import pyramid_tm
 
 from c2cwsgiutils import stats_pyramid, pyramid_logging, sql_profiler, version, debug, sentry,\
-    request_tracking, errors
+    request_tracking, errors, pretty_json
 
 
 def includeme(config):
@@ -15,6 +15,7 @@ def includeme(config):
     config.add_settings(handle_exceptions=False)
     config.include(pyramid_tm.includeme)
     config.include(cornice.includeme)
+    pretty_json.init(config)
     stats_pyramid.init(config)
     request_tracking.init(config)
     pyramid_logging.install_subscriber(config)

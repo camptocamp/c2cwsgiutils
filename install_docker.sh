c41de2b5cb35ae9552b7b35b271637972be759e2
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2017-11-10 09:17:55 +0100

    Make codacy happier

--- install_docker.sh
+++ install_docker.sh
@@ -2,14 +2,14 @@
 # Script used when building acceptance test images to install the docker tools needed to run them.
 set -e
 
-DEBIAN_VERSION=`grep VERSION= /etc/os-release | sed 's/.*(\(.*\)).\+/\1/'`
+DEBIAN_VERSION=$(grep VERSION= /etc/os-release | sed 's/.*(\(.*\)).\+/\1/')
 
 apt-get update
 apt-get install -y --no-install-recommends apt-transport-https
 
-if [[ ${DOCKER_VERSION} == *-ce ]]
+if [[ "${DOCKER_VERSION}" == *-ce ]]
 then
-    DOCKER_VERSION=$(echo "${DOCKER_VERSION}" | sed -e 's/\.[0-9]*\.[0-9]*-ce//')
+    DOCKER_VERSION=${DOCKER_VERSION//\.[0-9]*\.[0-9]*-ce/}
     BASE_URL="https://download.docker.com/linux/debian"
     curl -fsSL "${BASE_URL}/gpg" | apt-key add -
     echo "deb [arch=amd64] ${BASE_URL} ${DEBIAN_VERSION} stable" >> /etc/apt/sources.list
@@ -22,7 +22,7 @@ fi
 apt-get update
 apt-get install -y --no-install-recommends ${PACKAGE}=${DOCKER_VERSION}*
 
-curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" > /usr/bin/docker-compose
 chmod a+x /usr/bin/docker-compose
 
 apt-get clean

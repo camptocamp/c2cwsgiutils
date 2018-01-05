c9b7a971adc850d118794eaca7040f11d432bc14
Patrick Valsecchi <patrick@thus.ch>
2018-01-05 14:09:47 +0100

    Release 1.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = '1.3.0'
+VERSION = '1.4.0'
 HERE = os.path.abspath(os.path.dirname(__file__))
 INSTALL_REQUIRES = open(os.path.join(HERE, 'requirements.txt')).read().splitlines()
 

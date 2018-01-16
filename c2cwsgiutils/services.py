b2c3b094d9a13d29c98cf324bb5ccd9fcae55e33
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2018-01-16 09:55:06 +0100

    Allow to change the http_cache and cors config in services

--- c2cwsgiutils/services.py
+++ c2cwsgiutils/services.py
@@ -6,8 +6,8 @@ def create(name: str, path: str, *args: Any, **kwargs: Any) -> Service:
     """
     Create a cornice service with all the default configuration.
     """
-    kwargs['cors_origins'] = ["*"]
-    kwargs['cors_max_age'] = 86400
-    kwargs['depth'] = 2  # to make venusian find the good frame
-    kwargs['http_cache'] = 0  # disable client side and proxy caching
+    kwargs.setdefault('cors_origins', "*")
+    kwargs.setdefault('cors_max_age', 86400)
+    kwargs.setdefault('depth', 2)  # to make venusian find the good frame
+    kwargs.setdefault('http_cache', 0)  # disable client side and proxy caching by default
     return Service(name, path, *args, **kwargs)

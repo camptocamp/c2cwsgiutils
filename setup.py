3f7687b856c08cb51b90e339594eee4683568aab
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2018-05-01 17:02:20 +0200

    Release 1.11.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = '1.11.0'
+VERSION = '1.11.1'
 HERE = os.path.abspath(os.path.dirname(__file__))
 INSTALL_REQUIRES = [
     pkg.split('==')[0]

092ba1119d12768dd0880fa4fbf543c9d1e15554
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2018-06-11 15:06:31 +0200

    Fix codacy issues

--- tests/test_broadcast.py
+++ tests/test_broadcast.py
@@ -1,58 +1,59 @@
+import pytest
+
 from c2cwsgiutils.broadcast import local
 from c2cwsgiutils import broadcast
 
 
-def test_local():
-    broadcast._broadcaster = local.LocalBroadcaster()
+@pytest.yield_fixture()
+def local_broadcaster():
+    broadcast._broadcaster = local.LocalBroadcaster()  # pylint: disable=W0212
     try:
-        cb_calls = [0, 0]
-
-        def cb1(data):
-            cb_calls[0] += 1
-            return data + 1
-
-        def cb2(data):
-            cb_calls[1] += 1
-
-        assert broadcast.broadcast("test1", {'data': 1}, expect_answers=True) == []
-        assert cb_calls == [0, 0]
-
-        broadcast.subscribe("test1", cb1)
-        broadcast.subscribe("test2", cb2)
-        assert cb_calls == [0, 0]
-
-        assert broadcast.broadcast("test1", {'data': 1}) is None
-        assert cb_calls == [1, 0]
-
-        assert broadcast.broadcast("test2", {'data': 1}) is None
-        assert cb_calls == [1, 1]
-
-        assert broadcast.broadcast("test1", {'data': 12}, expect_answers=True) == [13]
-        assert cb_calls == [2, 1]
-
+        yield
     finally:
-        broadcast._broadcaster = None
+        broadcast._broadcaster = None  # pylint: disable=W0212
 
 
-def test_decorator():
-    broadcast._broadcaster = local.LocalBroadcaster()
-    try:
-        cb_calls = [0, 0]
+def test_local(local_broadcaster):
+    cb_calls = [0, 0]
 
-        @broadcast.decorator(expect_answers=True)
-        def cb1(value):
-            cb_calls[0] += 1
-            return value + 1
+    def cb1(data):
+        cb_calls[0] += 1
+        return data + 1
 
-        @broadcast.decorator()
-        def cb2():
-            cb_calls[1] += 1
+    def cb2(data):
+        cb_calls[1] += 1
 
-        assert cb1(value=12) == [13]
-        assert cb_calls == [1, 0]
+    assert broadcast.broadcast("test1", {'data': 1}, expect_answers=True) == []
+    assert cb_calls == [0, 0]
 
-        assert cb2() is None
-        assert cb_calls == [1, 1]
+    broadcast.subscribe("test1", cb1)
+    broadcast.subscribe("test2", cb2)
+    assert cb_calls == [0, 0]
 
-    finally:
-        broadcast._broadcaster = None
+    assert broadcast.broadcast("test1", {'data': 1}) is None
+    assert cb_calls == [1, 0]
+
+    assert broadcast.broadcast("test2", {'data': 1}) is None
+    assert cb_calls == [1, 1]
+
+    assert broadcast.broadcast("test1", {'data': 12}, expect_answers=True) == [13]
+    assert cb_calls == [2, 1]
+
+
+def test_decorator(local_broadcaster):
+    cb_calls = [0, 0]
+
+    @broadcast.decorator(expect_answers=True)
+    def cb1(value):
+        cb_calls[0] += 1
+        return value + 1
+
+    @broadcast.decorator()
+    def cb2():
+        cb_calls[1] += 1
+
+    assert cb1(value=12) == [13]
+    assert cb_calls == [1, 0]
+
+    assert cb2() is None
+    assert cb_calls == [1, 1]

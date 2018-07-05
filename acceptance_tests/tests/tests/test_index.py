42cd4ca2167d5ed18c6702b1b9037dc2528ebeed
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2018-07-05 07:28:25 +0200

    Test index behind a proxy doing https
    Without the `PasteDeploy#prefix` filter the URLs returned by `route_url`
    didn't have the https protocol when behind a proxy doing https. The
    `X-Forwarded-Proto` was not taken into account.
    
    This modification is only for showing how the applications using c2cwsgiutils
    should be configured.

--- acceptance_tests/tests/tests/test_index.py
+++ acceptance_tests/tests/tests/test_index.py
@@ -1,3 +1,6 @@
+from c2cwsgiutils.acceptance import utils
+
+
 def test_without_secret(app_connection):
     content = app_connection.get('c2c')
     assert "Health checks" in content
@@ -8,3 +11,10 @@ def test_with_secret(app_connection):
     content = app_connection.get('c2c', params={'secret': 'changeme'})
     assert "Health checks" in content
     assert "Debug" in content
+
+
+def test_https(app_connection):
+    content = app_connection.get('c2c', params={'secret': 'changeme'},
+                                 headers={'X-Forwarded-Proto': 'https'})
+    assert 'https://' + utils.DOCKER_GATEWAY + ':8480/api/' in content
+    assert 'http://' + utils.DOCKER_GATEWAY + ':8480/api/' not in content

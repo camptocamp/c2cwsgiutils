4ae8d90488c623157fc9e201c454d915ed197d81
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2018-08-02 14:15:35 +0200

    Release 2.9.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = '2.9.0'
+VERSION = '2.9.1'
 HERE = os.path.abspath(os.path.dirname(__file__))
 INSTALL_REQUIRES = [
     pkg.split('==')[0]

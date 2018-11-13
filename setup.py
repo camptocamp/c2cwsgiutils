606a60a89fe3420a7a2dbef32fa2b2419038f05b
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2018-11-13 13:01:40 +0100

    Release 2.14.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = '2.14.2'
+VERSION = '2.14.3'
 HERE = os.path.abspath(os.path.dirname(__file__))
 INSTALL_REQUIRES = [
     pkg.split('==')[0]

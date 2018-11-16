b47c2f2f26f73eedcd25dd89aeb110db575cb8f7
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2018-11-16 09:21:56 +0100

    Release 2.15.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = '2.14.4'
+VERSION = '2.15.0'
 HERE = os.path.abspath(os.path.dirname(__file__))
 INSTALL_REQUIRES = [
     pkg.split('==')[0]

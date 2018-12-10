df6d7c9fcbecdcea2da1a4ea490910ad8e594b8b
Stéphane Brunner <stephane.brunner@camptocamp.com>
2018-12-10 10:54:01 +0100

    Be able to don't specify env_name and config_name to dirrectly use the default current value

--- c2cwsgiutils/auth.py
+++ c2cwsgiutils/auth.py
@@ -1,5 +1,6 @@
 from pyramid.httpexceptions import HTTPForbidden
 import pyramid.request
+from typing import Optional
 
 # noinspection PyProtectedMember
 from c2cwsgiutils._utils import env_or_settings, env_or_config, config_bool
@@ -8,25 +9,32 @@ SECRET_PROP = 'c2c.secret'
 SECRET_ENV = 'C2C_SECRET'
 
 
-def _get_secret(settings: dict, env_name: str, config_name: str) -> str:
-    secret = env_or_settings(settings, env_name, config_name, None)
+def _get_secret(settings: dict, env_name: Optional[str]=None, config_name: Optional[str]=None) -> str:
+    secret = env_or_settings(settings, env_name, config_name) if \
+        env_name is not None or config_name is not None else None
     if secret is None:
         secret = env_or_settings(settings, SECRET_ENV, SECRET_PROP, False)
     return secret
 
 
-def is_auth(request: pyramid.request.Request, env_name: str, config_name: str) -> bool:
+def is_auth(
+        request: pyramid.request.Request,
+        env_name: Optional[str]=None, config_name: Optional[str]=None) -> bool:
     secret = request.params.get('secret')
     if secret is None:
         secret = request.headers.get('X-API-Key')
     return secret == _get_secret(request.registry.settings, env_name, config_name)
 
 
-def auth_view(request: pyramid.request.Request, env_name: str, config_name: str) -> None:
+def auth_view(
+        request: pyramid.request.Request,
+        env_name: Optional[str]=None, config_name: Optional[str]=None) -> None:
     if not is_auth(request, env_name, config_name):
         raise HTTPForbidden('Missing or invalid secret (parameter or X-API-Key header)')
 
 
-def is_enabled(config: pyramid.config.Configurator, env_name: str, config_name: str) -> bool:
+def is_enabled(
+        config: pyramid.config.Configurator,
+        env_name: Optional[str]=None, config_name: Optional[str]=None) -> bool:
     return config_bool(env_or_config(config, env_name, config_name)) and \
         env_or_config(config, SECRET_ENV, SECRET_PROP, '') != ''

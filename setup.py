1b4904718c2d53777c85f724bc9c15eb02426972
Stéphane Brunner <stephane.brunner@camptocamp.com>
2019-11-26 09:17:00 +0100

    Do the release 3.8.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = '3.8.3'
+VERSION = '3.8.4'
 HERE = os.path.abspath(os.path.dirname(__file__))
 INSTALL_REQUIRES = [
     pkg.split('==')[0]

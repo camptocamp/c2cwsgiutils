135f6e78060d97a2d43dc899eb734c182b273682
Patrick Valsecchi <patrick.valsecchi@camptocamp.com>
2019-12-16 10:45:17 +0100

    Fix for new version of coverage

--- c2cwsgiutils_coverage_report.py
+++ c2cwsgiutils_coverage_report.py
@@ -12,7 +12,8 @@ def main():
     dest_dir = '/tmp/coverage/api'
     shutil.rmtree(dest_dir, ignore_errors=True)
     shutil.copytree(report_dir, dest_dir)
-    cov = coverage.Coverage(data_file='coverage', data_suffix=True, source=sources, branch=True)
+    cov = coverage.Coverage(data_file=os.path.join(dest_dir, 'coverage'), data_suffix=True,
+                            source=sources or None, branch=True)
     cov.combine([dest_dir], strict=True)
     cov.html_report(directory=dest_dir, ignore_errors=True)
     cov.xml_report(outfile=os.path.join(dest_dir, 'coverage.xml'), ignore_errors=True)

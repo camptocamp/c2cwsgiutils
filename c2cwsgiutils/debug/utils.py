670b7f491ee910ba90162889689edb306bdd9775
Guillaume Beraudo <guillaume.beraudo@gmail.com>
2022-01-21 16:53:54 +0100

    Handle THPeligible smaps value

--- c2cwsgiutils/debug/utils.py
+++ c2cwsgiutils/debug/utils.py
@@ -53,6 +53,10 @@ def dump_memory_maps(pid: str = "self") -> List[Dict[str, Any]]:
                     name = matcher.group(1)
                     if name in ("Size", "Rss", "Pss"):
                         cur_dict[name.lower() + "_kb"] += int(matcher.group(2))
-                elif not line.startswith("VmFlags:") and not line.startswith("ProtectionKey:"):
+                elif (
+                    not line.startswith("VmFlags:")
+                    and not line.startswith("ProtectionKey:")
+                    and not line.startswith("THPeligible:")
+                ):
                     LOG.debug("Don't know how to parse /proc/%s/smaps line: %s", pid, line)
         return [{"name": name, **value} for name, value in sizes.items() if value.get("pss_kb", 0) > 0]

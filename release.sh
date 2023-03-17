f20fddbd3fd7babc8d0d66dc291a76f08d1228d8
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-03-17 16:12:24 +0100

    Fix new error with attr in pull request check workflow

--- release.sh
+++ release.sh
@@ -5,32 +5,28 @@ VERSION=$1
 cd /opt/c2cwsgiutils/
 actual_version=$(python3 ./setup.py --version 2> /dev/null)
 
-if [[ "${VERSION}" != "${actual_version}" ]]
-then
-    echo "Version mismatch: ${VERSION} != ${actual_version}"
-    exit 1
+if [[ "${VERSION}" != "${actual_version}" ]]; then
+  echo "Version mismatch: ${VERSION} != ${actual_version}"
+  exit 1
 fi
 
-if curl --silent --fail "https://pypi.org/project/c2cwsgiutils/${VERSION}/" > /dev/null
-then
-    echo "Already released ${VERSION}"
-    exit 0
+if curl --silent --fail "https://pypi.org/project/c2cwsgiutils/${VERSION}/" > /dev/null; then
+  echo "Already released ${VERSION}"
+  exit 0
 fi
 
 pip install -r publish-requirements.txt
 
 python3 ./setup.py bdist_wheel
 
-if twine upload -u "${USERNAME}" -p "${PASSWORD}" dist/*.whl
-then
-    echo "Upload to pypi successful"
+if twine upload -u "${USERNAME}" -p "${PASSWORD}" dist/*.whl; then
+  echo "Upload to pypi successful"
 else
-    # maybe it was uploaded at the same time by another job
-    if curl --silent --fail "https://pypi.org/project/c2cwsgiutils/${VERSION}/" > /dev/null
-    then
-        echo "Already released ${VERSION} in another job"
-        exit 0
-    else
-        exit 1
-    fi
+  # maybe it was uploaded at the same time by another job
+  if curl --silent --fail "https://pypi.org/project/c2cwsgiutils/${VERSION}/" > /dev/null; then
+    echo "Already released ${VERSION} in another job"
+    exit 0
+  else
+    exit 1
+  fi
 fi

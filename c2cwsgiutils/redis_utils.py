6d1aeaaa287120084cf57f3e69b14103df222451
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-08-24 15:52:17 +0200

    Allows rediss protocole and all others

--- c2cwsgiutils/redis_utils.py
+++ c2cwsgiutils/redis_utils.py
@@ -83,7 +83,7 @@ def _init(settings: Optional[Mapping[str, Any]]) -> None:
             print(_sentinel.sentinels[0].sentinel_masters())
             raise Exception(_sentinel.sentinels[0].sentinel_masters()) from error
     if url:
-        if not url.startswith("redis://"):
+        if "://" not in url:
             url = "redis://" + url
 
         LOG.info("Redis setup using: %s, with options: %s", url, redis_options_)

3c435d00524530644ec3ae120b87ae34315a86e2
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-11-29 11:42:03 +0100

    Skip test with too many false positive

--- acceptance_tests/tests/tests/test_db_maintenance.py
+++ acceptance_tests/tests/tests/test_db_maintenance.py
@@ -1,6 +1,8 @@
 import logging
 import time
 
+import pytest
+
 LOG = logging.getLogger(__name__)
 
 
@@ -18,6 +20,7 @@ def _query(app_connection, params, expected=None):
     assert response == all_expected
 
 
+@pytest.mark.skip(reason="Too many false positives")
 def test_api(app_connection):
     # _query(app_connection, {}, {"current_readonly": None})
     _query(app_connection, {"readonly": "true"})

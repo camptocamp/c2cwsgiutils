d02b15efa809cd9be41d8ba54806731fb2229407
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-08-06 15:51:58 +0200

    Fix test

--- acceptance_tests/tests/tests/test_scripts.py
+++ acceptance_tests/tests/tests/test_scripts.py
@@ -4,4 +4,4 @@ def test_get_hello(composition):
     logs = [
         o for o in logs if o != "The environment variable 'test' is duplicated with different case, ignoring"
     ]
-    assert logs[0] == "master"
+    assert len([o for o in logs if o == "master"]) == 1, "\n".join(logs)

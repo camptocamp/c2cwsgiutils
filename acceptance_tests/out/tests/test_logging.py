f75d4b369cc4089e6cf84005e8fb0417541a7890
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-08-06 15:55:23 +0200

    Use Docker Compose version 2

--- acceptance_tests/out/tests/test_logging.py
+++ acceptance_tests/out/tests/test_logging.py
@@ -9,7 +9,7 @@ def test_logs_request_id(app2_connection, composition):
     app2_connection.get_json("ping", headers={"X-Request-ID": "42 is the answer"})
     logs = composition.dc(["logs", "app2"]).split("\n")
     print("Got logs: " + repr(logs))
-    logs = [l for l in logs if re.search(r"\|.{4} \{", l)]
+    logs = [l for l in logs if re.search(r"\| \{", l)]
     logs = [json.loads(l[l.index("{") :]) for l in logs]
     logs = [l for l in logs if l["logger_name"] == "c2cwsgiutils_app.services.ping"]
     assert logs[-1]["request_id"] == "42 is the answer"

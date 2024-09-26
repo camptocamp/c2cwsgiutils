90a252879e07b059019649d0aafce38d8b2fe62d
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-09-26 10:09:18 +0200

    Make the versions view authenticated

--- acceptance_tests/tests/tests/test_versions.py
+++ acceptance_tests/tests/tests/test_versions.py
@@ -1,5 +1,6 @@
 def test_ok(app_connection):
-    response = app_connection.get_json("c2c/versions.json", cors=False)
+    params = {"secret": "changeme"}
+    response = app_connection.get_json("c2c/versions.json", params=params, cors=False)
     assert "main" in response
     assert "git_hash" in response["main"]
     assert "packages" in response

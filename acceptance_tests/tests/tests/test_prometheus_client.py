842b7df79abb46b80dbbdfda3bc7fcf617f478a6
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-11-24 10:16:52 +0100

    Fix logs in process commands

--- acceptance_tests/tests/tests/test_prometheus_client.py
+++ acceptance_tests/tests/tests/test_prometheus_client.py
@@ -4,16 +4,16 @@ _PID_RE = re.compile(r',pid="([0-9]+)"')
 
 
 def test_prometheus_1(prometheus_1_connection):
-    # One for the root process, one for each workers
+    # One for the root process, one for each workers (1)
     assert (
         len(set(_PID_RE.findall(prometheus_1_connection.get("metrics", cache_expected=False, cors=False))))
-        == 3
+        == 2
     )
 
 
 def test_prometheus_2(prometheus_2_connection):
     # One for the root process, one for each workers
-    assert (
-        len(set(_PID_RE.findall(prometheus_2_connection.get("metrics", cache_expected=False, cors=False))))
-        == 3
-    )
+    metrics = prometheus_2_connection.get("metrics", cache_expected=False, cors=False)
+    assert len(set(_PID_RE.findall(metrics))) == 0
+    assert re.search(r"^c2cwsgiutils_python_resource\{.*", metrics, re.MULTILINE) is not None
+    assert re.search(r"^c2cwsgiutils_python_memory_info\{.*", metrics, re.MULTILINE) is not None
--- c2cwsgiutils/loader.py
+++ c2cwsgiutils/loader.py
@@ -1,9 +1,9 @@
-import logging
+import logging.config
 from typing import Optional, cast
 
 from plaster_pastedeploy import Loader as BaseLoader
 
-from c2cwsgiutils import get_config_defaults
+from c2cwsgiutils import get_config_defaults, get_logconfig_dict
 
 _LOG = logging.getLogger(__name__)
 
@@ -19,3 +19,21 @@ class Loader(BaseLoader):  # type: ignore
     def __repr__(self) -> str:
         """Get the object representation."""
         return f'c2cwsgiutils.loader.Loader(uri="{self.uri}")'
+
+    def setup_logging(self, defaults: Optional[dict[str, str]] = None) -> None:
+        """
+        Set up logging via :func:`logging.config.fileConfig`.
+
+        Defaults are specified for the special ``__file__`` and ``here``
+        variables, similar to PasteDeploy config loading. Extra defaults can
+        optionally be specified as a dict in ``defaults``.
+
+        Arguments:
+            defaults: The defaults that will be used when passed to
+                :func:`logging.config.fileConfig`.
+
+        """
+        if "loggers" in self.get_sections():
+            logging.config.dictConfig(get_logconfig_dict(self.uri.path))
+        else:
+            logging.basicConfig()
